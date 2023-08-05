b08e1331fec147a7fc762fbfb89922604b580e50
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-08-05 16:15:29 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+               <img
+                   class="gallery__image"
+                   src="${preview}"
+                   data-source="${original}"
+                   alt="${description}"
+                 />
+           </a>
+        </li>`
+    )
+    .join("");
+}
+
+list.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
