5217afc08a51f5ea97f6d7bf920cfb8c27dc5f64
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-08-07 04:15:20 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href= "${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
 
-console.log(galleryItems);
+list.insertAdjacentHTML("beforeend", createMarkUp(galleryItems));
+list.addEventListener("click", handlerClick);
+
+console.log(SimpleLightbox);
+
+function handlerClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
